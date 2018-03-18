4a655b0563779b99b95f1fb9bd42abd7bbdda1ac
thomas <thomas.matecki@gmail.com>
2018-03-17 22:28:45 -0400

    google API authenticating

--- lib/google_client.dart
+++ lib/google_client.dart
@@ -1,12 +1,50 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart'
+    show BaseRequest, IOClient, Response, StreamedResponse;
+
+/// Google APIs:
+import 'package:googleapis/calendar/v3.dart';
+import 'package:googleapis/people/v1.dart';
+
+//import 'package:url_launcher/url_launcher.dart';
+//import 'package:googleapis_auth/auth_browser.dart';
+
+//import 'package:googleapis_auth/auth_io.dart';
+
 import 'dart:async';
+
+class GoogleHttpClient extends IOClient {
+  Map<String, String> _headers;
+
+  GoogleHttpClient(this._headers) : super();
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
+
+  @override
+  Future<Response> head(Object url, {Map<String, String> headers}) =>
+      super.head(url, headers: headers..addAll(_headers));
+}
 
 /// Singleton object for interacting with google APIs.
 ///
-/// The goals here is to support google sign-in on the
-/// client device, and also obtain authorization for
-/// any backend BrightPig services to access the google
+/// The goals here is to support google sign-in on the client device, and also
+/// obtain authorization for any backend BrightPig services to access the google
 /// APIs.
+///
+/// The Native APIs provide the ability to obtain a server side a `AuthCode`
+/// (e.g.  GoogleSignInOptions.Builder requestServerAuthCode) but this is not
+/// implemented in the flutter plugin.
+///
+/// see:  flutter/plugins/.../google_sign_in/android/
+///       io.flutter.plugins.googlesignin.GoogleSignInPlugin.java:208
+///
+/// Further more there is some dependency issue with the google-service gradle
+/// plugin and the boilerplate config, but example firebase_auth application
+/// somehow makes this work, so copying that should work.
+///
+///
 class GoogleClient {
   ///
   static GoogleSignIn _googleSignIn = new GoogleSignIn(
@@ -21,6 +59,11 @@ class GoogleClient {
   ///
   static GoogleSignInAccount _currentUser;
 
+  //GoogleHttpClient _httpClient;
+
+  CalendarApi calendarApi;
+  PeopleApi peopleApi;
+
   ///
   static final _instance = new GoogleClient._internal();
 
@@ -29,19 +72,7 @@ class GoogleClient {
   factory GoogleClient() {
     _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
       _currentUser = account;
-
-      print(account.displayName);
-
-      ///
-      account.authentication.then((GoogleSignInAuthentication auth) {
-
-        // ARG!
-        // flutter/plugins/blob/master/packages/google_sign_in/android/src/main/java/io/flutter/plugins/googlesignin/
-        //
-        // GoogleSignInPlugin.java:208 ?!?!?!
-        print("Id Token: ${auth.idToken}");
-        print("Access Token: ${auth.accessToken}");
-      });
+      _instance._syncHeaders(account.authHeaders);
     });
 
     GoogleClient.doGooglesignIn();
@@ -49,14 +80,26 @@ class GoogleClient {
     return _instance;
   }
 
+  void _syncHeaders(Future<Map<String, String>> authHeaders) async {
+    final headers = await authHeaders;
+
+    final httpClient = new GoogleHttpClient(headers);
+    this.calendarApi = new CalendarApi(httpClient);
+    this.peopleApi = new PeopleApi(httpClient);
+
+    var now = new DateTime.now().toUtc();
+
+    var data = await calendarApi.events.list("primary",
+        singleEvents: true, orderBy: 'startTime', maxResults: 10, timeMin: now);
+
+    print(data);
+  }
+
   ///
   static Future<Null> doGooglesignIn() async {
     try {
       await _googleSignIn.signOut();
       await _googleSignIn.signIn();
-
-      final GoogleSignInAuthentication auth =
-          await _googleSignIn.currentUser.authentication;
     } catch (error) {
       print("Error signing in");
       print(error);

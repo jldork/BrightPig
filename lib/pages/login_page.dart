d320516d09559c5d3a0b38ed453d4b9d87a8a745
Thomas Matecki <thomas.matecki@gmail.com>
2018-05-01 15:30:25 -0400

    [Feature]begin linkedin OAuth flow

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import '../widgets/primary_button.dart';
 import '../constants/colors.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key key, this.loginFn}) : super(key: key);
 
   final Function loginFn;
+  // For Linkedin Login
+  final flutterWebviewPlugin = new FlutterWebviewPlugin(); 
   
   static GoogleSignIn _googleSignIn = new GoogleSignIn(
     scopes: <String>[
@@ -17,9 +20,22 @@ class LoginPage extends StatelessWidget {
     ],
   );
 
+
+
   void _googleLogin() async {
     await _googleSignIn.signIn().then((account){
       loginFn(account, 'google');
+    }).then((onValue){
+   
+      String client_id = '77guos8d5wo4kp';
+      String client_secret = 'sYfL02XUgoY7V3t9';
+      String redirect_uri = 'https://us-central1-brightpig-197813.cloudfunctions.net/auth-linkedin';
+
+      String url = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&state=987654321&scope=r_basicprofile";
+      
+      flutterWebviewPlugin.launch(url);
+
+
     });
   }
 
